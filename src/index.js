ecd38ae4a819f56597b8b7401e703edd85aec836
Uday Odedara <udayodibusiness@gmail.com>
2024-12-29 12:45:24 +0530

    feat: api utils setup

--- src/index.js
+++ src/index.js
@@ -3,26 +3,34 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongoDB connection failed", err);
+  });
 
 /*
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.error("App Error: ", error);
+  import express from "express";
+  const app = express();
+  
+  (async () => {
+    try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+      app.on("error", (error) => {
+        console.error("App Error: ", error);
+        throw error;
+      });
+      app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+      });
+    } catch (error) {
+      console.error("ERROR: ", error);
       throw error;
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR: ", error);
-    throw error;
-  }
-})();
-
-*/
+    }
+  })();
+  
+  */
